c0b9a518510c74bf193ec09e6bdd73bd1ec18095
unknown <1668924806@qq.com>
2016-08-08 10:32:00 +0800

    second_commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import {List, Map} from 'immutable';
+
+/*const pair = List.of('Trainspotting', '28DaysLater');
+const tally = Map({'Trainspotting': 5, '28DaysLater': 4});
+
+export default React.createClass({
+	render: function() {
+		return React.cloneElement(this.props.children, 
+			{
+				pair: pair,
+				tally: tally
+			}
+			);
+	}
+});*/
+
+
+/*export default React.createClass({
+	render: function() {
+		return React.cloneElement(this.props.children);
+	}
+});*/
+
+export default React.createClass({
+	render: function() {
+		return this.props.children;
+	}
+});
---
+++ src/index.jsx
@@ -0,0 +1,61 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+import {Route,Router,hashHistory} from 'react-router';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import io from 'socket.io-client';
+import {setState} from './action_creators';
+
+//import Voting from './components/Voting';
+import {VotingContainer} from './components/Voting';
+import App from './components/App';
+//import Results from './components/Results';
+import {ResultsContainer} from './components/Results';
+import reducer from './reducer';
+import remoteActionMiddleware from './remote_action_middleware';
+
+require('./css/style.css');
+
+//const pair = ['Trainspotting', '28DaysLater'];
+
+//const store = createStore(reducer);
+
+
+
+/*store.dispatch({
+	type: 'SET_STATE',
+	state: {
+		vote: {
+			pair: ['Sunshine', '28DaysLater'],
+			tally: {Sunshine: 2}
+		}
+	}
+});*/
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state => 
+	//store.dispatch({type: 'SET_STATE', state})
+	store.dispatch(setState(state))
+	);
+
+const createStoreWithMiddleware = applyMiddleware(
+	remoteActionMiddleware(socket)
+	)(createStore);
+const store = createStoreWithMiddleware(reducer);
+
+const routes = <Route component={App}>
+	<Route path="/" component={VotingContainer} />
+	<Route path="/results" component={ResultsContainer} />
+</Route>;
+
+/*ReactDOM.render(
+	<Voting pair={pair} />,
+	document.getElementById('app')
+	);*/
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={hashHistory}>{routes}</Router>
+	</Provider>,
+	document.getElementById('app')
+	)
